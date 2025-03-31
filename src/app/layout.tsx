31787bd0ee7c8463d5b57f64a10b79b34b1c11cd
ompathak2004 <om2004.pathak@gmail.com>
2025-03-31 18:21:52 +0530

    Updated: Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
+
 import "./globals.css";
 import ClientBody from "./ClientBody";
 
@@ -30,6 +32,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <ClientBody>
         {children}
+        <Analytics/>
       </ClientBody>
     </html>
   );
